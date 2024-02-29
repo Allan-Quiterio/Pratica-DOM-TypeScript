a0c6884a4a3301aba15dec3848e5de439d6d57fe
Allan-Quiterio <allanquiterio100@gmail.com>
2024-02-29 18:43:02 -0300

    feat: show slide

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,8 @@ const container = document.getElementById("slide");
 const elements = document.getElementById("slide-elements");
 const controls = document.getElementById("slide-controls");
 if (container && elements && controls && elements.children.length) {
-    new Slide(container, Array.from(elements.children), controls, 3000);
+    const slide = new Slide(container, Array.from(elements.children), controls, 3000);
+    slide.show(3);
+    console.log(slide.slide);
 }
 //# sourceMappingURL=script.js.map
