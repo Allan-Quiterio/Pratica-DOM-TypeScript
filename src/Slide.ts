6843d3e03976ba558bd45b43d03c009945a6c261
Allan-Quiterio <allanquiterio100@gmail.com>
2024-03-01 18:23:15 -0300

    feat: video element

--- src/Slide.ts
+++ src/Slide.ts
@@ -20,7 +20,9 @@ export default class Slide {
     this.slides = slides;
     this.controls = controls;
     this.time = time;
-    this.index = 0;
+    this.index = localStorage.getItem("activeSlide")
+      ? Number(localStorage.getItem("activeSlide"))
+      : 0;
     this.slide = this.slides[this.index];
     this.timeout = null;
     this.paused = false;
@@ -30,13 +32,33 @@ export default class Slide {
   }
   hide(el: Element) {
     el.classList.remove("active");
+    if (el instanceof HTMLVideoElement) {
+      el.currentTime = 0;
+      el.pause();
+    }
   }
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
+    localStorage.setItem("activeSlide", String(this.index));
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
-    this.auto(this.time);
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autoVideo(this.slide);
+    } else {
+      this.auto(this.time);
+    }
+  }
+  autoVideo(video: HTMLVideoElement) {
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener("playing", () => {
+      if (firstPlay) {
+        this.auto(video.duration * 1000);
+        firstPlay = false;
+      }
+    });
   }
   auto(time: number) {
     this.timeout?.clear();
@@ -56,6 +78,7 @@ export default class Slide {
     this.timeout?.pause();
     this.pausedTimeout = new Timeout(() => {
       this.paused = true;
+      if (this.slide instanceof HTMLVideoElement) this.slide.pause();
     }, 300);
   }
   continue() {
@@ -64,6 +87,7 @@ export default class Slide {
     if (this.paused) {
       this.paused = false;
       this.timeout?.continue();
+      if (this.slide instanceof HTMLVideoElement) this.slide.play();
     }
   }
   private addControls() {
